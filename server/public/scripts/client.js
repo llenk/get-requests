27a7aab11a7cce09ef5ed8f1da375eb9ebe830a6
llenk <ellenkeal@gmail.com>
2018-04-19 17:09:39 -0500

    append quotes to DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,6 +2,11 @@ console.log('client.js is loaded');
 $(document).ready(onReady);
 
 function onReady() {
+    displayStartupQuotes();
+    $('#submit').on('click', addQuote);
+}
+
+function displayStartupQuotes() {
     displayQuote();
     console.log('jQuery is loaded');
     $.ajax({
@@ -27,11 +32,41 @@ function displayQuote() {
 }
 
 function displayAllQuotes() {
+    $('#allQuotes').empty();
     $.ajax({
         type: 'GET', //does not need to be all caps, but convention
         url: '/all-quotes'
     })
     .then(function(response) {
-        $('#allQuotes').append(response);
+        $('#allQuotes').append(formatQuotesList(response));
     });
 }
+
+function addQuote() {
+    let quote = {
+        quote: $('#quoteInput').val(),
+        author: $('#authorInput').val()
+    };
+    console.log(quote);
+    $('#quoteInput').val('');
+    $('#authorInput').val('');
+    $.ajax({
+        method: 'POST',
+        url: '/add-quote',
+        data: quote
+    }).then(function(response) {
+        displayAllQuotes();
+    });
+}
+
+function formatQuotesList(quotes_data) {
+    let quotesString = '<ul>';
+    quoteStringMaker = (item) => {
+        quotesString += '<li>"' + item.quote + '" -' + item.author + '</li>';
+    }
+    
+    quotes_data.forEach(quoteStringMaker);
+    quotesString += '</ul>';
+    return quotesString;
+}
+
--- server/server.js
+++ server/server.js
@@ -1,31 +1,30 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
 
-const quotes_data = [
-    { quote: 'I\'m not going to school just for the academics - I wanted to share ideas, to be around people who are passionate about learning.', author: 'Emma Watson' },
-    { quote: 'Remember there\'s no such thing as a small act of kindness. Every act creates a ripple with no logical end.', author: 'Scott Adams' },
-    { quote: 'Intelligence plus character-that is the goal of true education.', author: 'Martin Luther King, Jr.' }
-];
+const quotes_data = require('./modules/quotes_data');
 
 app.listen(PORT, function () {
     console.log(`listening on port ${PORT}`);
 });
 
 app.use(express.static('server/public'));
-
-let quotesString = '<ul>';
-quoteStringMaker = (item) => {
-    quotesString += '<li>"' + item.quote + '" -' + item.author + '</li>';
-}
-
-quotes_data.forEach(quoteStringMaker);
-quotesString += '</ul>';
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/all-quotes', function(req, res) {
-    res.send(quotesString);
+    res.send(quotes_data);
 });
+
+app.get('/quote', function(req, res) {
+    res.send(randomQuote());
+});
+
+app.post('/add-quote', function(req, res) {
+    console.log(req.body);
+    quotes_data.push(req.body);
+    res.sendStatus(200);
+})
 
 randomQuote = () => {
     let i = Math.floor(Math.random()*3);
@@ -33,7 +32,3 @@ randomQuote = () => {
     returnString = '"' + quote.quote + '" -' + quote.author;
     return returnString;
 }
-
-app.get('/quote', function(req, res) {
-    res.send(randomQuote());
-});
